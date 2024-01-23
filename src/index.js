265a8ed2e5188cfd17214e524c36d6dcf9ed3cb3
Teo <teocyp1@gmail.com>
2024-01-23 15:54:02 +0200

    Star Component Created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        {/* <App /> */}
+        <StarRating
+            maxRating={5}
+            messages={["terrible", "bad", "okay", "good", "amazing"]}
+        />
+        <StarRating maxRating={12} color="purple" size={28} />
     </React.StrictMode>
 );
