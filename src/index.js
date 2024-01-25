80a5dd6bd5430511ae5315ff846f4c3aec4e84f3
Teo <teocyp1@gmail.com>
2024-01-25 15:40:59 +0200

    Fetch Movies Functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <StarRating
+        <App />
+        {/* <StarRating
             maxRating={5}
             messages={["terrible", "bad", "okay", "good", "amazing"]}
-        />
-        <StarRating maxRating={12} color="purple" size={28} />
+        /> */}
     </React.StrictMode>
 );
